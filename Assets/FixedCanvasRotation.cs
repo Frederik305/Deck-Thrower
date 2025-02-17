d9a0b77cf08a7f09ddc451894be33489d00f7414
Logan <129766393+Logansenechal@users.noreply.github.com>
2025-02-17 10:50:36 -0500

    Update FixedCanvasRotation.cs

--- Assets/FixedCanvasRotation.cs
+++ Assets/FixedCanvasRotation.cs
@@ -4,6 +4,7 @@ public class FixedCanvasRotation : MonoBehaviour
 {
     public Transform player; // Référence au player (ou à l'objet qui tourne)
 
+    //LS
     void Update()
     {
         // Fixer la rotation du Canvas (par exemple, toujours face à la caméra)
