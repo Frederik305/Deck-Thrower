395b31e432de37fe958cd02abc8b1ff16738d85c
Frederik Pigeon <fredprivate@duck.com>
2025-02-20 14:13:29 -0500

    more precise detection

---
+++ Assets/Scripts/DetectPlayer.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DetectPlayer : MonoBehaviour
+{
+    [SerializeField] private Tilemap tilemap;
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Vector3 playerPosition = other.transform.position;
+
+            Vector3Int tilePosition = tilemap.WorldToCell(playerPosition);
+
+            // Get the tile at that position
+            TileBase tile = tilemap.GetTile(tilePosition);
+
+            Debug.Log("Player passed through a door at " + tilePosition);
+        }
+    }
+}
