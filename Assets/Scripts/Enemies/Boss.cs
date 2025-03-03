cae1e2fcfbdb57ae2686ea2950a7719f1238c9c2
Jules Prince-Legault <JulesPrinceLegault@gmail.com>
2025-03-03 09:53:13 -0500

    Retiration de code inutile en commentaire
    Retiration de code inutile en commentaire

--- Assets/Scripts/Enemies/Boss.cs
+++ Assets/Scripts/Enemies/Boss.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class Enemy : MonoBehaviour
+public class Boss : MonoBehaviour
 {
     //The enemy's health
-    public int health = 10;
+    public int health = 50;
 
     protected GameManager gameManager;
     public Slider healthBar; // Référence au Slider (barre de vie)
     protected ScoreManager scoreManager;
+    //public float deactivationDistance = 20f; // Distance maximale avant désactivation
+    //public float reactivationDistance = 18f; // Distance minimale avant réactivation
     private Transform player;
     private Rigidbody2D rb;
 
@@ -26,6 +28,23 @@ public class Enemy : MonoBehaviour
         {
             healthBar.maxValue = health;  // Définir la vie maximale
             healthBar.value = health;     // Définir la vie actuelle
+        }
+    }
+
+    private void Update()
+    {
+        if (player != null)
+        {
+            /*float distance = Vector2.Distance(transform.position, player.position);
+            if (distance > deactivationDistance)
+            {
+                rb.linearVelocity = Vector2.zero; // Arrêter l'ennemi
+                rb.angularVelocity = 0f;   // Arrêter la rotation
+            }
+            else if (distance < reactivationDistance && rb.linearVelocity == Vector2.zero)
+            {
+                rb.linearVelocity = transform.up * 2f; // Redonner une vitesse de base à l'ennemi
+            }*/
         }
     }
 
